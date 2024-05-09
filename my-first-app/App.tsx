af59dc107109e75c262ca65129c3b42a7f88b618
Hailey Kim <kfbkhw@gmail.com>
2024-05-09 21:30:12 +0900

    ✨ make a simple counter app

--- my-first-app/App.tsx
+++ my-first-app/App.tsx
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+    const [number, setNumber] = useState<number>(0);
+
+    const handleDecrement = () => {
+        const newNumber = number - 1;
+        setNumber(newNumber);
+    };
+
+    const handleIncrement = () => {
+        const newNumber = number + 1;
+        setNumber(newNumber);
+    };
+
     return (
         <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
-            <StatusBar style="auto" />
+            <Text style={styles.title}>Counter</Text>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                    onPress={handleDecrement}
+                    style={styles.button}
+                >
+                    <Text style={styles.buttonText}>-</Text>
+                </TouchableOpacity>
+                <Text style={styles.content}>{number}</Text>
+                <TouchableOpacity
+                    onPress={handleIncrement}
+                    style={styles.button}
+                >
+                    <Text style={styles.buttonText}>+</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
@@ -13,8 +39,30 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
+        gap: 60,
+        backgroundColor: '#fff',
     },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    buttonContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 40,
+    },
+    content: { fontSize: 50 },
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#75A47F',
+        padding: 6,
+        paddingHorizontal: 20,
+        borderRadius: 6,
+    },
+    buttonText: { fontSize: 30, color: '#fff' },
 });
