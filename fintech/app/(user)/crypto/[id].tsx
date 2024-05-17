e4222e3f6e0f2007a0804d84387ed824bcafb4d5
Hailey Kim <kfbkhw@gmail.com>
2024-05-17 22:30:23 +0900

    ⚡️ add Circle & Text on CryptoChart, enhance display onload

--- fintech/app/(user)/crypto/[id].tsx
+++ fintech/app/(user)/crypto/[id].tsx
@@ -47,193 +47,202 @@ export default function CryptoDetailScreen() {
 
     return (
         <>
-            <Stack.Screen
-                options={{
-                    title: crypto?.name,
-                    headerStyle: {
-                        backgroundColor: Colors.light,
-                    },
-                }}
-            />
-            <SectionList
-                style={{
-                    marginTop: headerHeight,
-                    backgroundColor: Colors.light,
-                }}
-                contentInsetAdjustmentBehavior="automatic"
-                keyExtractor={(i) => i.title}
-                sections={[{ data: [{ title: 'Chart' }] }]}
-                renderSectionHeader={() => (
-                    <ScrollView
-                        horizontal={true}
-                        showsHorizontalScrollIndicator={false}
-                        contentContainerStyle={{
-                            flex: 1,
-                            alignItems: 'center',
-                            justifyContent: 'space-between',
-                            paddingHorizontal: 16,
-                            paddingVertical: 8,
-                            borderBottomColor: Colors.secondaryMuted,
-                            borderBottomWidth: StyleSheet.hairlineWidth,
+            {crypto && (
+                <>
+                    <Stack.Screen
+                        options={{
+                            title: crypto?.name,
+                            headerStyle: {
+                                backgroundColor: Colors.light,
+                            },
+                        }}
+                    />
+                    <SectionList
+                        style={{
+                            marginTop: headerHeight,
                             backgroundColor: Colors.light,
                         }}
-                    >
-                        {categories.map((item, index) => (
-                            <TouchableOpacity
-                                key={index}
-                                onPress={() => setActiveIndex(index)}
-                                style={[
-                                    {
+                        contentInsetAdjustmentBehavior="automatic"
+                        keyExtractor={(i) => i.title}
+                        sections={[{ data: [{ title: 'Chart' }] }]}
+                        renderSectionHeader={() => (
+                            <ScrollView
+                                horizontal={true}
+                                showsHorizontalScrollIndicator={false}
+                                contentContainerStyle={{
+                                    flex: 1,
+                                    alignItems: 'center',
+                                    justifyContent: 'space-between',
+                                    paddingHorizontal: 16,
+                                    paddingVertical: 8,
+                                    borderBottomColor: Colors.secondaryMuted,
+                                    borderBottomWidth: StyleSheet.hairlineWidth,
+                                    backgroundColor: Colors.light,
+                                }}
+                            >
+                                {categories.map((item, index) => (
+                                    <TouchableOpacity
+                                        key={index}
+                                        onPress={() => setActiveIndex(index)}
+                                        style={[
+                                            {
+                                                alignItems: 'center',
+                                                justifyContent: 'center',
+                                                padding: 10,
+                                                paddingHorizontal: 14,
+                                                borderRadius: 17,
+                                            },
+                                            activeIndex === index && {
+                                                backgroundColor: Colors.white,
+                                            },
+                                        ]}
+                                    >
+                                        <Text
+                                            style={[
+                                                { fontSize: 14 },
+                                                activeIndex === index
+                                                    ? { color: Colors.dark }
+                                                    : {
+                                                          color: Colors.secondary,
+                                                      },
+                                            ]}
+                                        >
+                                            {item}
+                                        </Text>
+                                    </TouchableOpacity>
+                                ))}
+                            </ScrollView>
+                        )}
+                        ListHeaderComponent={() => (
+                            <>
+                                <View
+                                    style={{
+                                        flexDirection: 'row',
                                         alignItems: 'center',
-                                        justifyContent: 'center',
-                                        padding: 10,
-                                        paddingHorizontal: 14,
-                                        borderRadius: 17,
-                                    },
-                                    activeIndex === index && {
+                                        justifyContent: 'space-between',
+                                        marginHorizontal: 16,
+                                    }}
+                                >
+                                    <Text
+                                        style={{
+                                            marginBottom: 20,
+                                            fontSize: 20,
+                                            fontWeight: '700',
+                                            color: Colors.secondary,
+                                        }}
+                                    >
+                                        {crypto?.symbol}
+                                    </Text>
+                                    <Image
+                                        source={{ uri: crypto?.logo }}
+                                        style={{ width: 60, height: 60 }}
+                                    />
+                                </View>
+                                <View
+                                    style={{
+                                        flexDirection: 'row',
+                                        gap: 10,
+                                        margin: 12,
+                                        marginBottom: 24,
+                                    }}
+                                >
+                                    <TouchableOpacity
+                                        style={{
+                                            flexDirection: 'row',
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                            gap: 8,
+                                            height: 40,
+                                            paddingLeft: 16,
+                                            paddingRight: 20,
+                                            borderRadius: 20,
+                                            backgroundColor: Colors.primary,
+                                        }}
+                                    >
+                                        <Ionicons
+                                            name="add"
+                                            size={24}
+                                            color={Colors.light}
+                                        />
+                                        <Text
+                                            style={{
+                                                fontSize: 18,
+                                                fontWeight: '500',
+                                                color: Colors.light,
+                                            }}
+                                        >
+                                            {Message.cryptoDetailAction1}
+                                        </Text>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity
+                                        style={{
+                                            flexDirection: 'row',
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                            gap: 8,
+                                            height: 40,
+                                            paddingLeft: 16,
+                                            paddingRight: 20,
+                                            borderRadius: 20,
+                                            backgroundColor:
+                                                Colors.primaryMuted,
+                                        }}
+                                    >
+                                        <Ionicons
+                                            name="checkmark"
+                                            size={24}
+                                            color={Colors.primary}
+                                        />
+                                        <Text
+                                            style={{
+                                                fontSize: 18,
+                                                fontWeight: '500',
+                                                color: Colors.primary,
+                                            }}
+                                        >
+                                            {Message.cryptoDetailAction2}
+                                        </Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </>
+                        )}
+                        renderItem={() => (
+                            <>
+                                {tickers?.[0]?.price && (
+                                    <CryptoChart tickers={tickers} />
+                                )}
+                                <View
+                                    style={{
+                                        gap: 16,
+                                        margin: 20,
+                                        padding: 14,
+                                        borderRadius: 16,
                                         backgroundColor: Colors.white,
-                                    },
-                                ]}
-                            >
-                                <Text
-                                    style={[
-                                        { fontSize: 14 },
-                                        activeIndex === index
-                                            ? { color: Colors.dark }
-                                            : { color: Colors.secondary },
-                                    ]}
-                                >
-                                    {item}
-                                </Text>
-                            </TouchableOpacity>
-                        ))}
-                    </ScrollView>
-                )}
-                ListHeaderComponent={() => (
-                    <>
-                        <View
-                            style={{
-                                flexDirection: 'row',
-                                alignItems: 'center',
-                                justifyContent: 'space-between',
-                                marginHorizontal: 16,
-                            }}
-                        >
-                            <Text
-                                style={{
-                                    marginBottom: 20,
-                                    fontSize: 20,
-                                    fontWeight: '700',
-                                    color: Colors.secondary,
-                                }}
-                            >
-                                {crypto?.symbol}
-                            </Text>
-                            <Image
-                                source={{ uri: crypto?.logo }}
-                                style={{ width: 60, height: 60 }}
-                            />
-                        </View>
-                        <View
-                            style={{
-                                flexDirection: 'row',
-                                gap: 10,
-                                margin: 12,
-                                marginBottom: 24,
-                            }}
-                        >
-                            <TouchableOpacity
-                                style={{
-                                    flexDirection: 'row',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    gap: 8,
-                                    height: 40,
-                                    paddingLeft: 16,
-                                    paddingRight: 20,
-                                    borderRadius: 20,
-                                    backgroundColor: Colors.primary,
-                                }}
-                            >
-                                <Ionicons
-                                    name="add"
-                                    size={24}
-                                    color={Colors.light}
-                                />
-                                <Text
-                                    style={{
-                                        fontSize: 18,
-                                        fontWeight: '500',
-                                        color: Colors.light,
                                     }}
                                 >
-                                    {Message.cryptoDetailAction1}
-                                </Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity
-                                style={{
-                                    flexDirection: 'row',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    gap: 8,
-                                    height: 40,
-                                    paddingLeft: 16,
-                                    paddingRight: 20,
-                                    borderRadius: 20,
-                                    backgroundColor: Colors.primaryMuted,
-                                }}
-                            >
-                                <Ionicons
-                                    name="checkmark"
-                                    size={24}
-                                    color={Colors.primary}
-                                />
-                                <Text
-                                    style={{
-                                        fontSize: 18,
-                                        fontWeight: '500',
-                                        color: Colors.primary,
-                                    }}
-                                >
-                                    {Message.cryptoDetailAction2}
-                                </Text>
-                            </TouchableOpacity>
-                        </View>
-                    </>
-                )}
-                renderItem={() => (
-                    <>
-                        {tickers && <CryptoChart tickers={tickers} />}
-                        <View
-                            style={{
-                                gap: 16,
-                                margin: 20,
-                                padding: 14,
-                                borderRadius: 16,
-                                backgroundColor: Colors.white,
-                            }}
-                        >
-                            <Text
-                                style={{
-                                    fontSize: 20,
-                                    fontWeight: '700',
-                                    color: Colors.dark,
-                                }}
-                            >
-                                {Message.cryptoDetailOverviewTitle}
-                            </Text>
-                            <Text
-                                style={{
-                                    color: Colors.secondary,
-                                    lineHeight: 18,
-                                }}
-                            >
-                                {crypto?.description}
-                            </Text>
-                        </View>
-                    </>
-                )}
-            ></SectionList>
+                                    <Text
+                                        style={{
+                                            fontSize: 20,
+                                            fontWeight: '700',
+                                            color: Colors.dark,
+                                        }}
+                                    >
+                                        {Message.cryptoDetailOverviewTitle}
+                                    </Text>
+                                    <Text
+                                        style={{
+                                            color: Colors.secondary,
+                                            lineHeight: 18,
+                                        }}
+                                    >
+                                        {crypto?.description}
+                                    </Text>
+                                </View>
+                            </>
+                        )}
+                    ></SectionList>
+                </>
+            )}
         </>
     );
 }
--- fintech/components/CryptoChart.tsx
+++ fintech/components/CryptoChart.tsx
@@ -1,18 +1,43 @@
-import {
-    View,
-    Text,
-    SectionList,
-    StyleSheet,
-    Image,
-    TouchableOpacity,
-    ScrollView,
-} from 'react-native';
-import { CartesianChart, Line } from 'victory-native';
+import { View, Text, TextInput } from 'react-native';
+import { CartesianChart, Line, useChartPressState } from 'victory-native';
+import { Circle, useFont } from '@shopify/react-native-skia';
+import Animated, { useAnimatedProps } from 'react-native-reanimated';
+import type { SharedValue } from 'react-native-reanimated';
 import Colors from '@/constants/Colors';
-import { useFont } from '@shopify/react-native-skia';
+
+Animated.addWhitelistedNativeProps({ text: true });
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
+
+function ToolTip({ x, y }: { x: SharedValue<number>; y: SharedValue<number> }) {
+    return (
+        <Circle
+            cx={x}
+            cy={y}
+            r={8}
+            color={Colors.primary}
+            blendMode={'difference'}
+        />
+    );
+}
 
 export default function CryptoChart({ tickers }: { tickers: any[] }) {
     const font = useFont(require('@/assets/fonts/Inter-Regular.ttf'), 10);
+    const { state, isActive } = useChartPressState({ x: 0, y: { price: 0 } });
+
+    const animatedText = useAnimatedProps(() => {
+        return {
+            text: `${state.y.price.value.value.toFixed(2)} $`,
+            defaultValue: '',
+        };
+    });
+
+    const animatedDateText = useAnimatedProps(() => {
+        const date = new Date(state.x.value.value);
+        return {
+            text: `${date.toLocaleDateString()}`,
+            defaultValue: '',
+        };
+    });
 
     return (
         <View
@@ -27,7 +52,47 @@ export default function CryptoChart({ tickers }: { tickers: any[] }) {
                 backgroundColor: Colors.white,
             }}
         >
+            <View style={{ marginBottom: 20, marginLeft: 10 }}>
+                {isActive ? (
+                    <>
+                        <AnimatedTextInput
+                            editable={false}
+                            underlineColorAndroid={'transparent'}
+                            style={{
+                                fontSize: 30,
+                                fontWeight: 'bold',
+                                color: Colors.dark,
+                            }}
+                            animatedProps={animatedText}
+                        />
+                        <AnimatedTextInput
+                            editable={false}
+                            underlineColorAndroid={'transparent'}
+                            style={{ fontSize: 18, color: Colors.secondary }}
+                            animatedProps={animatedDateText}
+                        />
+                    </>
+                ) : (
+                    <>
+                        <Text
+                            style={{
+                                fontSize: 30,
+                                fontWeight: '700',
+                                color: Colors.dark,
+                            }}
+                        >
+                            {`${tickers[tickers.length - 1].price.toFixed(
+                                2
+                            )} $`}
+                        </Text>
+                        <Text style={{ fontSize: 18, color: Colors.secondary }}>
+                            {'Today'}
+                        </Text>
+                    </>
+                )}
+            </View>
             <CartesianChart
+                chartPressState={state}
                 axisOptions={{
                     font,
                     tickCount: 5,
@@ -49,11 +114,19 @@ export default function CryptoChart({ tickers }: { tickers: any[] }) {
                 xKey="timestamp"
                 yKeys={['price']}
                 children={({ points }) => (
-                    <Line
-                        points={points.price}
-                        color={Colors.primary}
-                        strokeWidth={3}
-                    />
+                    <>
+                        <Line
+                            points={points.price}
+                            color={Colors.primary}
+                            strokeWidth={3}
+                        />
+                        {isActive && (
+                            <ToolTip
+                                x={state.x.position}
+                                y={state.y.price.position}
+                            />
+                        )}
+                    </>
                 )}
             />
         </View>
